f4536f04f1d3a24a349a5ec1f76fb2326f0d149a
f321x <f321x@tutamail.com>
2023-10-24 23:00:17 +0200

    payments

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ import os
 import uuid
 from database import initialize_db, add_row, delete_row, fetch_row_by_uuid, update_single_column
 from threading import Thread
-from make_sting import get_menu_for_week
+from make_string import get_menu_for_week
+from payment import getinvoice, checkinvoice
 
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 app = Flask(__name__)
 initialize_db()
+
+payment_status = {}
+payments_enabled = bool(os.getenv('payments'))
 
 
 def food_api_request():
@@ -37,8 +41,8 @@ def food_api_request():
 
 def ai_generation(diet_plan: str, food_plan):
     food_plan_formatted = get_menu_for_week(food_plan)
-    prompt = f"Generate a {diet_plan} diet plan for this week, only lunch, composed of the following available meals: {food_plan_formatted}"
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
+    prompt = f"Generate a {diet_plan} lunch diet plan for this week, composed of the following available meals which are available for lunch at the mensa: {food_plan_formatted}"
+    completion = openai.ChatCompletion.create(model="gpt-4", messages=[{"role": "user", "content": prompt}])
     print(str(completion.choices[0].message.content))
     return str(completion.choices[0].message.content)
 
@@ -53,17 +57,32 @@ def run_generation(diet_plan: str, uuid_identifier: str):
     update_single_column(uuid_identifier, "content", result)
 
 
+def clean_old_payments(payment_dictionary):
+    time_now = int(time.time())
+    keys_to_remove = []
+    for key, value in payment_dictionary.items():
+        if (time_now - value) > 1200:
+            keys_to_remove.append(key)
+    for key in keys_to_remove:
+        del payment_dictionary[key]
+
+
 @app.route("/request-diet-plan", methods=['POST'])
 def handler():
     if request.method == 'POST':
         try:
             uuid_identifier = str(uuid.uuid1())
             diet_preference = request.get_json()
-            thread = Thread(target=run_generation, args=(diet_preference['diet'], uuid_identifier,))
-            thread.start()
-            return json.dumps({"uuid": uuid_identifier}), 200
+            if checkinvoice(diet_preference['payment_hash']) or payments_enabled is False:
+                if payments_enabled is True:
+                    del payment_status[diet_preference['payment_hash']]
+                thread = Thread(target=run_generation, args=(diet_preference['diet'], uuid_identifier,))
+                thread.start()
+                return json.dumps({"uuid": uuid_identifier}), 200
+            else:
+                return "", 402
         except KeyError:
-            return 400
+            return "", 400
 
 
 @app.route("/request-diet-plan-status/<request_uuid>", methods=['GET'])
@@ -80,9 +99,26 @@ def get_generation_status(request_uuid):
     else:
         return "", 400
 
-@app.route("/web", methods=['GET'])
+
+@app.route("/get-invoice", methods=['GET'])
+def get_invoice():
+    if payments_enabled is True:
+        invoice = getinvoice()
+        return_json = {'payment_request': invoice['payment_request'],
+                       'payment_hash': invoice['payment_hash']}
+        payment_status[invoice['payment_hash']] = int(time.time())
+    else:
+        return_json = {'payment_request': 'foo',
+                       'payment_hash': 'bar'}
+    return json.dumps(return_json), 200
+
+
+@app.route("/", methods=['GET'])
 def web_interface():
+    if payments_enabled is True:
+        clean_old_payments(payment_status)
     return render_template('index.html')
 
 
-# app.run(port=5556)
+app.run(port=5556)
+# print(getinvoice())
---
+++ payment.py
@@ -0,0 +1,23 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+headers = {
+    'X-Api-Key': os.environ['lnbits_invoice_key'],
+    'Content-type': 'application/json',
+}
+
+data = '{"out": false, "amount":100, "memo":"Mensa Diet Generator", "unit":"sat"}'
+
+
+def getinvoice():
+    response = requests.post('https://legend.lnbits.com/api/v1/payments', headers=headers, data=data)
+    return response.json()
+
+
+def checkinvoice(payment_hash):
+    response = requests.get('https://legend.lnbits.com/api/v1/payments/' + payment_hash, headers=headers)
+    return response.json()['paid']
